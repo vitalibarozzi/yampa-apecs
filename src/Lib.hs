426da436e2dc2f5d8aed03003512b4eb2eb5ca7e
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2024-05-14 00:44:41 -0300

    working?

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,58 +29,63 @@ import Data.TreeDiff.Pretty
 import Data.IORef
 import Control.Concurrent.MVar
 import Linear    as L
-
-instance (Eq a, Floating a) => VectorSpace (V2 a) a where
-  zeroVector = L.zero
-  (*^) = (L.*^)
-  --(^) = (L.^)
-  negateVector = L.negated
-  (^+^) = (L.^+^)
-  (^-^) = (L.^-^)
-  dot = L.dot
-
-
-data Foo = Foo Int String deriving (Generic,ToExpr)
+import Unsafe.Coerce
 
 
 
-data Input  -- all types of events
-data Output -- all those backend models
-runGame ::
-    SF Input (System w Output) ->
-    --System w (SF Input Output) ->
-    IO ()
-runGame game = do
-    undefined
 
-
-newtype Position = Position (V2 Double) deriving Show
-newtype Velocity = Velocity (V2 Double) deriving Show
-data Flying = Flying
-type SecretType = SF Input Position
-makeWorldAndComponents "Asteroids" [''Position, ''Velocity, ''Flying]
-
-
-runPositionSystem = do
-    ref <- liftIO $ newIORef (Position 0)
-    let positionSF = \(Position x0, Velocity v0) -> 
-                            proc input -> do 
-                                (x1 :: V2 Double) <- (arr (+x0) <<< integral) -< v0
-                                (returnA :: SF a a) -< Position x1
-    handle <- liftIO $ reactInit (pure NoEvent) (\handle updated pos -> when updated (writeIORef ref pos) >> pure False) (proc ev -> do drSwitch (positionSF (Position 0, Velocity 0)) -< ((), ev))
-    forever do
-        liftIO (threadDelay 100000)
-        cmapSF ref handle 0.1 positionSF
-        cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p)
-
-cmapSF ref handle dt fsf = do
+-----------------------------------------------------------
+cmapSF (ref, handle) dt fsf = do
     cmapM 
         $ \components -> do
             liftIO do
-                _ <- react handle (dt, Just (Event (fsf components)))
+                -- TODO this is need to make it work, not sure why. find out
+                _ <- react handle (0, Just (Event (fsf components)))
+                _ <- react handle (dt, Nothing)
                 readIORef ref
 
 
+-----------------------------------------------------------
+reactive ::
+    (t -> SF () b) ->
+    t ->
+    b ->
+    System w (Payload x b)
+reactive initSF initialState initialInput =
+    liftIO do
+        ref    <- newIORef initialInput
+        handle <- reactInit (pure NoEvent) (actuate ref) switcher
+        return (ref,unsafeCoerce $ handle)
+  where
+    actuate ref handle updated pos = do
+        when updated (writeIORef ref pos) 
+        pure updated
+    switcher = proc ev -> do 
+        drSwitch (initSF initialState) -< ((), ev)
+
+
+type Payload a b = (IORef b, ReactHandle (Event a) ())
+
+
+-----------------------------------------------------------------
+-------------------- usage ----------------------------------
+
+newtype Position = Position (V2 Double) deriving Show
+newtype Velocity = Velocity (V2 Double) deriving Show
+
+data Flying = Flying
+
+makeWorldAndComponents "Asteroids" [''Position, ''Velocity, ''Flying]
+
+
+-----------------------------------------------------------
+myGame :: Payload (SF a Position) Position -> System Asteroids ()
+myGame h = do
+    liftIO (threadDelay 10000)
+    cmapSF h 0.01 position
+
+
+-----------------------------------------------------------
 someFunc :: IO ()
 someFunc = do
     asteroids <- initAsteroids
@@ -90,5 +95,39 @@ someFunc = do
         newEntity (Position 0, Velocity 1)
         newEntity (Position 2, Velocity 1)
         newEntity (Position 1, Velocity 2)
-        runPositionSystem
-    --print (prettyEditExpr $ ediff (toExpr $ Foo 2 "opa") (toExpr $ Foo 0 "opa"))
+
+        cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p)
+
+        h <- reactive position (Position 0, Velocity 0) (Position 0) 
+
+        --forever (myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p)))
+
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+        newEntity (Position 100, Velocity 1)
+
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+        newEntity (Position 10, Velocity 1)
+
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+        myGame h >> (cmapM_ $ \(Position p, Entity e) -> liftIO . print $ (e, p))
+
+
+-----------------------------------------------------------
+position :: (Position, Velocity) -> SF a Position 
+position (Position x0, Velocity v0) =
+    proc input -> do 
+        x1 <- (arr (+x0) <<< integral) -< v0
+        returnA -< Position x1
+
+
+-----------------------------------------------------------
+instance (Eq a, Floating a) => VectorSpace (V2 a) a where
+  zeroVector = L.zero
+  (*^) = (L.*^)
+  --(^) = (L.^)
+  negateVector = L.negated
+  (^+^) = (L.^+^)
+  (^-^) = (L.^-^)
+  dot = L.dot
