b75162c4022ac019525bf90b4967ea1a2c63c37c
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2024-05-23 00:33:49 -0300

    wip

--- src/Apecs/Yampa.hs
+++ src/Apecs/Yampa.hs
@@ -1,83 +1,127 @@
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE BangPatterns     #-}
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE BlockArguments     #-}
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-module Apecs.Yampa 
-    ( cmapSF,
-      reactInitSF,
-      ApecsHandle,
-    )
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Apecs.Yampa (
+    cmapSF,
+    ApecsHandle,
+    reactiveEntt,
+    reactiveComp,
+    -- makeWorldAndComponents_,
+)
 where
 
-
 import Apecs
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.IORef
 import FRP.Yampa hiding (reactInit)
 import qualified FRP.Yampa as Yampa
-import Control.Monad.IO.Class
-import Control.Monad
-import Data.IORef
-
+import Language.Haskell.TH
 
 -----------------------------------------------------------
--- | Internal type.
-newtype ApecsHandle cx cy = ApecsHandle 
-    ( IORef cy
-    , ReactHandle (cx, Event (SF cx cy)) cy
-    )
 
+{- | Convenience function so we can pass the types directly
+makeWorldAndComponents_ :: String -> [String] -> Q [Dec]
+makeWorldAndComponents_ world namesStr = do
+   let names = fmap undefined namesStr
+   makeWorldAndComponents world names
+-}
 
 -----------------------------------------------------------
--- | Constructor.
-reactInitSF :: (MonadIO m) => SF cx cy -> cx -> SystemT w m (ApecsHandle cx cy)
-{-# INLINE reactInitSF #-}
-reactInitSF !sf cx = do
+
+-- | Convenience function.
+reactiveEntt ::
+    ( MonadIO m
+    , Set w m cx
+    , Has w m EntityCounter
+    ) =>
+    SystemT w m cx ->
+    SystemT w m ()
+{-# INLINE reactiveEntt #-}
+reactiveEntt initStuff = do
+    sfs <- initStuff
+    newEntity_ sfs
+
+-----------------------------------------------------------
+
+reactiveComp ::
+    forall cx cy w m.
+    (MonadIO m) =>
+    SF cx cy ->
+    cy ->
+    SystemT w m (cy, ApecsHandle cx cy)
+{-# INLINE reactiveComp #-}
+reactiveComp !sf !initialValue = do
     liftIO do
         !ref <- newIORef (error "never called")
-        !hdl <- Yampa.reactInit (pure (cx, Event sf)) (actuate ref) (drSwitch sf)
-        return (ApecsHandle (ref, hdl))
+        !hdl <- Yampa.reactInit setup (actuate ref) (wrap (drSwitch sf))
+        threadDelay 1
+        return (initialValue, ApecsHandle (ref, hdl))
   where
+    setup = pure (Nothing, NoEvent)
+
     actuate !ref _ !updated cy = do
-        when updated (writeIORef ref cy) 
+        when updated (writeIORef ref cy)
         pure updated
 
+    wrap :: SF (cx, Event (SF cx cy)) cy -> SF (Maybe cx, Event (SF cx cy)) cy
+    wrap !ss = proc (!mx, !ev) -> do
+        case mx of
+            Just !x -> ss -< (x, ev)
+            Nothing -> returnA -< initialValue
 
 -----------------------------------------------------------
--- | Use an SF to map all entities that have the components
--- cx and cy associated with a handler.
-cmapSF :: 
-    forall w m cx cy. 
 
+{- | Use an SF to map all entities that have the components
+cx and cy associated with a handler.
+-}
+cmapSF ::
+    forall w m cx cy.
     ( Members w m cx
-    , Get     w m cx
-
+    , Get w m cx
     , Members w m cy
-    , Get     w m cy
-    , Set     w m cy
-
+    , Get w m cy
+    , Set w m cy
     , Members w m (ApecsHandle cx cy)
-    , Get     w m (ApecsHandle cx cy)
+    , Get w m (ApecsHandle cx cy)
     , MonadIO m
-    ) => 
-    DTime -> 
-    SF cx cy -> 
+    ) =>
+    DTime ->
+    SF cx cy ->
     SystemT w m ()
-{-# INLINABLE cmapSF #-}
+{-# INLINEABLE cmapSF #-}
 cmapSF dt sf = do
-    cmapM 
+    cmapM
         \( ApecsHandle (ref, hdl) :: ApecsHandle cx cy
-         , cx                     :: cx
-         , cy                     :: cy
-         ) -> 
-        liftIO do
-            updated1 <- react hdl (0, Just (cx, Event sf))
-            if updated1
-               then do
-                    !updated2 <- react hdl (dt, Nothing)
-                    if updated2
-                       then readIORef ref
-                       else pure cy
-               else pure cy
+            , cx :: cx
+            , cy :: cy
+            ) ->
+                liftIO do
+                    updated1 <- react hdl (0, Just (Just cx, Event sf))
+                    if updated1
+                        then do
+                            !updated2 <- react hdl (dt, Nothing)
+                            if updated2
+                                then readIORef ref
+                                else pure cy
+                        else pure cy
+
+-----------------------------------------------------------
+
+{- | A Yampa handle wrapped. Double-duty to speed up Apecs
+as it makes the component more specific when we use it to
+create a new entity. See Apecs.
+-}
+newtype ApecsHandle cx cy
+    = ApecsHandle
+        ( IORef cy
+        , ReactHandle (Maybe cx, Event (SF cx cy)) cy
+        )
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,39 +1,41 @@
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE BlockArguments     #-}
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE Arrows     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
-import Apecs.Yampa
 import Apecs
-import FRP.Yampa
-import Data.Text as Text
-import Linear.V2
-import Linear    as L
+import Apecs.Yampa
 import Control.Concurrent
 import Control.Monad
-
-
------------------------------------------------------------
-newtype Position = Position (V2 Double) deriving Show
-
-
------------------------------------------------------------
-newtype Velocity = Velocity (V2 Double) deriving Show
-
-
-type Foo = ApecsHandle (Position,Velocity) Position
+import Data.Text as Text
+import FRP.Yampa
+import Linear as L
+import Linear.V2
 
 -----------------------------------------------------------
-makeWorldAndComponents "World" [''Position, ''Velocity, ''Foo]
+newtype Position = Position (V2 Double) deriving (Show)
 
+-----------------------------------------------------------
+newtype Velocity = Velocity (V2 Double) deriving (Show)
+
+-----------------------------------------------------------
+type PosHandle = ApecsHandle (Position, Velocity) Position
+
+-----------------------------------------------------------
+makeWorldAndComponents
+    "World"
+    [ ''Position
+    , ''Velocity
+    , ''PosHandle
+    ]
 
 -----------------------------------------------------------
 main :: IO ()
@@ -43,31 +45,31 @@ main = do
   where
     myApp :: System World ()
     myApp = do
-
         let foo = cmapM \(Position p) -> liftIO (putStrLn $ show p <> "\n")
-        
-        entityHdl <- reactInitSF positionSF (Position 0, Velocity 0)
-        newEntity_ (Position 0, Velocity 1, entityHdl)
 
-        cmapSF 10    positionSF >> foo
-        cmapSF 10.1  positionSF >> foo
-        cmapSF 0     positionSF >> foo
+        reactiveEntt do
+            entityHdl <- reactiveComp positionSF (Position 0)
+            return (Velocity 1, entityHdl)
+
+        cmapSF 10 positionSF >> foo
+        cmapSF 10.1 positionSF >> foo
+        cmapSF 0 positionSF >> foo
         cmapSF (-13) positionSF >> foo
-        cmapSF 13    positionSF >> foo
+        cmapSF 13 positionSF >> foo
 
-    positionSF :: SF (Position, Velocity) Position 
+    positionSF :: SF (Position, Velocity) Position
     positionSF =
-        proc (Position x0, Velocity v0) -> do 
+        proc (Position x0, Velocity v0) -> do
             dx <- integral -< v0
             returnA -< Position (x0 + dx)
 
-
 -----------------------------------------------------------
 instance (Eq a, Floating a) => VectorSpace (V2 a) a where
-  zeroVector = L.zero
-  (*^) = (L.*^)
-  --(^) = (L.^)
-  negateVector = L.negated
-  (^+^) = (L.^+^)
-  (^-^) = (L.^-^)
-  dot = L.dot
+    zeroVector = L.zero
+    (*^) = (L.*^)
+
+    -- (^) = (L.^)
+    negateVector = L.negated
+    (^+^) = (L.^+^)
+    (^-^) = (L.^-^)
+    dot = L.dot
